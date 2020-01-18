be576de6fb404e5c90214d34dc273277d66ee536
danil <5699320@gmail.com>
2020-01-18 15:24:49 +0200

    base configuration

---
+++ presentation/src/main/java/com/reddit/presentation/feature/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.reddit.presentation.feature.splash
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created with Android Studio.
+ * User: Danil Konovalenko
+ * Date: 2020-01-18
+ * Time: 13:48
+ */
+class SplashActivity : AppCompatActivity() {
+
+}
